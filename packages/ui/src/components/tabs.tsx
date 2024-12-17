fb30d496ebf20199dbf2a0c1cdb7e774bddc5bb0
Lukas Bach <contact@lukasbach.com>
2024-12-17 02:37:25 +0100

    nicer tabs

--- packages/ui/src/components/tabs.tsx
+++ packages/ui/src/components/tabs.tsx
@@ -23,7 +23,10 @@ export const TabsTriggerVisual: FC<
       "px-4 pt-2 pb-2 mr-0.5",
       "rounded-tl rounded-tr font-bold shadow-topglow-1 text-sm",
       [active, "bg-blackLight translate-y-[4px] text-textBeige"],
-      [!active, "bg-grayLight text-black"],
+      [
+        !active,
+        "bg-grayLight text-black hover:bg-orangeDark hover:shadow-orangeglow",
+      ],
     )}
     {...props}
   />
@@ -34,10 +37,11 @@ export const TabsTrigger: FC<
 > = (props) => {
   return (
     <Tabs.Trigger
-      className={[
+      className={combine(
         "bg-grayLight  data-[state=active]:bg-blackLight data-[state=active]:translate-y-[4px] px-4 pt-2 pb-2 mr-0.5",
         "rounded-tl rounded-tr font-bold shadow-topglow-1 text-black data-[state=active]:text-textBeige text-sm",
-      ].join(" ")}
+        "hover:bg-orangeDark data-[state=inactive]:hover:shadow-orangeglow",
+      )}
       {...props}
       ref={undefined}
     />
