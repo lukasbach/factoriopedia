893ce7b0080a9822815c49cdd7f1d4123ebcd279
Lukas Bach <contact@lukasbach.com>
2024-12-17 17:45:09 +0100

    disable fusiontool

--- packages/pedia/src/tools/index.ts
+++ packages/pedia/src/tools/index.ts
@@ -2,4 +2,4 @@ export * from "./heatlist";
 export * from "./unithealth";
 // export * from "./solarratio";
 export * from "./fuels";
-export * from "./fusionratio";
+// TODO export * from "./fusionratio";
