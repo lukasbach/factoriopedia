84dbc826d34760e21e111a0170c71345de461513
Lukas Bach <contact@lukasbach.com>
2024-12-18 22:35:27 +0100

    disable tooltips on mobile

--- packages/ui/src/components/tooltip-root.tsx
+++ packages/ui/src/components/tooltip-root.tsx
@@ -1,13 +1,22 @@
 import { Tooltip as ReactTooltip } from "react-tooltip";
 
-export const TooltipRoot = () => (
-  <ReactTooltip
-    float
-    offset={24}
-    noArrow
-    className="!p-0 max-w-64 z-[1000]"
-    place="bottom-start"
-    id="factorio-ui-tooltip"
-    delayShow={300}
-  />
-);
+function isTouchDevice() {
+  return (
+    "ontouchstart" in window ||
+    (navigator as any).maxTouchPoints > 0 ||
+    (navigator as any).msMaxTouchPoints > 0
+  );
+}
+
+export const TooltipRoot = () =>
+  isTouchDevice() ? null : (
+    <ReactTooltip
+      float
+      offset={24}
+      noArrow
+      className="!p-0 max-w-64 z-[1000]"
+      place="bottom-start"
+      id="factorio-ui-tooltip"
+      delayShow={300}
+    />
+  );
